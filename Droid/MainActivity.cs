3d3a5c00f1e049ea7178ef119bbbe02d0603557e
hdknr <gmail@hdknr.com>
2016-02-20 13:42:22 +0900

    Droid Renderer == ViewGroup

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,10 @@ using Android.OS;
 
 namespace MobileCache.Droid
 {
-	[Activity (Label = "MobileCache.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "MobileCache.Droid", 
+		Icon = "@drawable/icon", 
+		MainLauncher = true, 
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
---
+++ Droid/MainContentRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+
+
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+
+[assembly:ExportRenderer(
+	typeof(MobileCache.MainContent), 
+	typeof(MobileCache.Droid.MainContentRenderer))]
+
+
+namespace MobileCache.Droid
+{
+	public class MainContentRenderer : PageRenderer	// ViewGroup
+	{
+		public MainContentRenderer ()
+		{
+			var activity = this.Context as Activity;
+		}
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Page> e)
+		{
+			base.OnElementChanged (e);
+
+		}
+	}
+}
+
